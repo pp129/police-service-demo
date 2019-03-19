0d97ef35ab4ac0f7b8ff00abb485a4e6dece50a9
panfei <604042454@qq.com>
2019-03-19 18:12:56 +0800

    case

--- src/views/case.vue
+++ src/views/case.vue
@@ -24,12 +24,12 @@ export default {
                 effect: "coverflow",
                 grabCursor: true,
                 centeredSlides: false,
-                slidesPerView: 3.5,
+                slidesPerView: 3,
                 loop: true,
                 coverflowEffect: {
-                    rotate: 50,
-                    stretch: 0,
-                    depth: 100,
+                    rotate: 40,
+                    stretch: 80,
+                    depth: 150,
                     modifier: 1,
                     slideShadows: false
                 }
